e777c542ecab9d6b7c972f102c3bb2b27ffa880a
acarl005 <acarl005@g.ucla.edu>
2015-04-28 19:25:01 -0700

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@ categories = [
   'community'
 ]
 
-# categories.each do |name|
-#   Category.create(name: name)
-# end
+categories.each do |name|
+  Category.find_or_create_by(name: name)
+end
 
 categories.each do |name|
   Category.where(name: name).first.articles << Article.create(title: 'fake', body: 'lorem ipsum', contact: 'Andy')
